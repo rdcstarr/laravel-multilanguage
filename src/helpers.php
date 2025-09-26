3a117a4d6a7bb1daf79dbb3af565cc9ed583cfc9
Rdcstarr <rdcstarr@gmail.com>
2025-09-26 09:00:23 +0300

    1b27

--- src/helpers.php
+++ src/helpers.php
@@ -21,3 +21,25 @@ if (!function_exists('localedata'))
 		return $localedata->get($key, $default);
 	}
 }
+
+if (!function_exists('_ld'))
+{
+	/**
+	 * Get app localedata value or instance
+	 *
+	 * @param string|null $key
+	 * @param mixed $default
+	 * @return mixed
+	 */
+	function _ld(?string $key = null, mixed $default = ''): mixed
+	{
+		$localedata = app('localedata');
+
+		if ($key === null)
+		{
+			return $localedata;
+		}
+
+		return $localedata->get($key, $default);
+	}
+}
